173a6c6e4724f8dc3473df8ec9f2904f681cb939
mihoutao18 <138785638+mihoutao1868@users.noreply.github.com>
2025-04-03 08:44:32 +0800

    Create hm.js

---
+++ hm.js
@@ -0,0 +1,14 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?b02339e46f047b26d7adb7f56456681d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?e27d398e2203ffb8028e3bcea4bba14a";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
