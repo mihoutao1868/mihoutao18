830988ca4c805edea61945927de2d7432d5d3c4f
mihoutao18 <138785638+mihoutao1868@users.noreply.github.com>
2025-04-06 05:49:15 +0800

    Create 618.js

---
+++ 618.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ef38641ebb469b284504a86ae9274532";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
