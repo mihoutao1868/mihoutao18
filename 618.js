0bad33fb99e23ff1bf3bf43a82063c65592b6e04
mihoutao18 <138785638+mihoutao1868@users.noreply.github.com>
2025-04-15 02:09:51 +0800

    Update 618.js

--- 618.js
+++ 618.js
@@ -1,2 +1,8 @@
-/*! 合作TG:@AK81688 */
-document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"KmGPxrPRybwbovg9\",ck:\"KmGPxrPRybwbovg9\"});</script>")
+/*! Telegram:@AK81688 */
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?affb52b6666a36ebc0612c5a239c9ad7";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
