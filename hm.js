6593e96f788d080aa10659892ecd0460a80423ee
mihoutao18 <138785638+mihoutao1868@users.noreply.github.com>
2025-04-15 02:10:45 +0800

    Update hm.js

--- hm.js
+++ hm.js
@@ -1,4 +1,4 @@
-/*! 合作TG:@AK81688 */
+/*! Telegram:@AK81688 */
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
@@ -6,4 +6,10 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
-document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"KmGPeo61EAybXz1Q\",ck:\"KmGPeo61EAybXz1Q\"});</script>")
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?1f407fee808bd414e311105ae4ce100e";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
