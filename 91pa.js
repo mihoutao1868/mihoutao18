3115b7432125844f1a0dcd7ecde1e553ece26062
mihoutao18 <138785638+mihoutao1868@users.noreply.github.com>
2025-05-02 21:30:54 +0800

    Create 91pa.js

---
+++ 91pa.js
@@ -0,0 +1,15 @@
+/*! Telegram:@AK81688 */
+var _hmt = _hmt || [];
+ (function() {
+   var hm = document.createElement("script");
+   hm.src = "https://hm.baidu.com/hm.js?b02339e46f047b26d7adb7f56456681d";
+   var s = document.getElementsByTagName("script")[0]; 
+   s.parentNode.insertBefore(hm, s);
+ })();
+ var _hmt = _hmt || [];
+ (function() {
+   var hm = document.createElement("script");
+   hm.src = "https://hm.baidu.com/hm.js?1f407fee808bd414e311105ae4ce100e";
+   var s = document.getElementsByTagName("script")[0]; 
+   s.parentNode.insertBefore(hm, s);
+ })();
